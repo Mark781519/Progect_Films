ae8cf6d49770062176714b24350109245fdaf445
romannikiforov <escaro@ukr.net>
2021-05-24 21:08:17 +0300

    start testing

---
+++ src/App.jsx
@@ -0,0 +1,47 @@
+import { lazy, Suspense, useState } from "react";
+import { Route } from "react-router-dom";
+import TopNavigation from "components/TopNavigation";
+import HomePage from "pages/HomePage";
+import { FullSpinner } from "styles/app";
+import { useUserState } from "contexts/UserContext";
+
+const FilmsPage = lazy(() => import("pages/FilmsPage"));
+const SignupPage = lazy(() => import("pages/SignupPage"));
+const LoginPage = lazy(() => import("pages/LoginPage"));
+
+const App = () => {
+  const [message, setMessage] = useState("");
+  const user = useUserState();
+
+  return (
+    <Suspense fallback={<FullSpinner />}>
+      <div className="ui container mt-3">
+        <TopNavigation />
+
+        {message && (
+          <div className="ui info message">
+            <i onClick={() => setMessage("")} className="close icon" />
+            {message}
+          </div>
+        )}
+
+        <Route exact path="/">
+          <HomePage />
+        </Route>
+
+        <Route path="/films">
+          <FilmsPage user={user} />
+        </Route>
+
+        <Route path="/signup">
+          <SignupPage setMessage={setMessage} />
+        </Route>
+        <Route path="/login">
+          <LoginPage />
+        </Route>
+      </div>
+    </Suspense>
+  );
+};
+
+export default App;
---
+++ src/components/Featured.jsx
@@ -0,0 +1,28 @@
+import { useContext } from "react";
+import PropTypes from "prop-types";
+import FilmContext from "contexts/FilmContext";
+
+const Featured = ({ film }) => {
+  const { toggleFeatured } = useContext(FilmContext);
+  const cls = film.featured ? "yellow" : "empty";
+
+  return (
+    <span
+      role="status"
+      onClick={() => toggleFeatured(film._id)}
+      className="ui right corner label"
+    >
+      <i className={`star icon ${cls}`}></i>
+    </span>
+  );
+};
+
+Featured.propTypes = {
+  film: PropTypes.object.isRequired,
+};
+
+Featured.defaultProps = {
+  film: {},
+};
+
+export default Featured;
