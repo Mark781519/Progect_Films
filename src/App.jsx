5f54f09e27f969b79ffbc20de455fa1b55b0779a
roman <escaro@ukr.net>
2021-06-04 21:20:21 +0300

    ready snippet 6 message

--- src/App.jsx
+++ src/App.jsx
@@ -15,24 +15,40 @@ const initUser = {
 class App extends Component {
   state = {
     user: initUser,
+    message: "",
   };
 
   logout = () => this.setState({ user: { token: null } });
 
+  setMessage = (message) => this.setState({ message });
+
   render() {
-    const { user } = this.state;
+    const { user, message } = this.state;
 
     return (
       <Suspense fallback={<FullSpinner />}>
         <div className="ui container mt-3">
           <TopNavigation logout={this.logout} isAuth={user.token} />
+
+          {message && (
+            <div className="ui info message">
+              <i
+                onClick={() => this.setState({ message: "" })}
+                className="close icon"
+              />
+              {message}
+            </div>
+          )}
+
           <Route exact path="/">
             <HomePage />
           </Route>
           <Route path="/films">
             <FilmsPage />
           </Route>
-          <Route path="/signup" component={SignupPage} />
+          <Route path="/signup">
+            <SignupPage setMessage={this.setMessage} />
+          </Route>
           <Route path="/login" component={LoginPage} />
         </div>
       </Suspense>
