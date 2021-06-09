0d48eacfdd5365c42e3f6c184dffa59358307a06
roman <escaro@ukr.net>
2021-06-09 21:13:10 +0300

    start test submit

--- src/pages/LoginPage/__tests__/LoginForm.spec.jsx
+++ src/pages/LoginPage/__tests__/LoginForm.spec.jsx
@@ -1,8 +1,31 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import { MemoryRouter as Router } from "react-router-dom";
+import { render, screen, fireEvent } from "@testing-library/react";
 import LoginForm from "pages/LoginPage/components/LoginForm";
+import faker from "faker";
+
+const buildFormData = (overiddes) => ({
+  email: faker.internet.email(),
+  password: faker.internet.password(),
+  ...overiddes,
+});
 
 test("should correct render LoginForm", () => {
-  const div = document.createElement("div");
-  ReactDOM.render(<LoginForm />, div);
+  render(
+    <Router>
+      <LoginForm />
+    </Router>
+  );
+  const { email, password } = buildFormData();
+
+  const emailEl = screen.getByLabelText(/email/i);
+  const passwordEl = screen.getByLabelText(/password/i);
+  const btnEl = screen.getByText(/login/i);
+
+  fireEvent.change(emailEl, { target: { value: email } });
+  fireEvent.change(passwordEl, { target: { value: password } });
+
+  expect(emailEl).toHaveValue(email);
+  expect(passwordEl).toHaveValue(password);
+
+  fireEvent.click(btnEl);
 });
