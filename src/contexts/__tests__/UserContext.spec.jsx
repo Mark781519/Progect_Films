0279f1ddc082efad907987574b5922e255bed109
roman <escaro@ukr.net>
2021-06-14 19:18:46 +0300

    ready all tests UserContext

--- src/contexts/__tests__/UserContext.spec.jsx
+++ src/contexts/__tests__/UserContext.spec.jsx
@@ -1,5 +1,5 @@
 import { renderHook, act } from "@testing-library/react-hooks";
-import { useLogin, UserContextProvider } from "contexts/UserContext";
+import { useLogin, useLogout, UserContextProvider } from "contexts/UserContext";
 import jwtDecode from "jwt-decode";
 import { setAuthorizationHeader } from "api";
 
@@ -12,6 +12,11 @@ jest.mock("jwt-decode", () => jest.fn());
 
 jest.mock("api");
 
+afterEach(() => {
+  jest.resetAllMocks();
+  delete localStorage.filmsToken;
+});
+
 test("useLogin should return function", async () => {
   jwtDecode.mockImplementation(() => ({ user: {} }));
   const { result } = renderHook(() => useLogin(), { wrapper });
@@ -23,4 +28,15 @@ test("useLogin should return function", async () => {
 
   expect(setAuthorizationHeader).toHaveBeenCalledTimes(1);
   expect(localStorage.filmsToken).toBe(mockToken);
+});
+
+test("logout should remove mockToken from localStorage", () => {
+  jwtDecode.mockImplementation(() => ({ user: {} }));
+  const { result } = renderHook(() => useLogout(), { wrapper });
+
+  act(() => result.current(mockToken));
+
+  expect(setAuthorizationHeader).toHaveBeenCalledTimes(1);
+  expect(localStorage.filmsToken).not.toBe(mockToken);
+  expect(localStorage.filmsToken).toBe(undefined);
 });
